32aa3a0f853523208d21a3d0e1c2c5b193471ee5
sophia <feiyang.f@gmail.com>
2024-09-06 11:24:56 -0700

    Integrate openai & set up chat streaming

--- app.py
+++ app.py
@@ -1,10 +1,34 @@
 import chainlit as cl
+import openai
+import os
+
+api_key = os.getenv("OPENAI_API_KEY")
+
+endpoint_url = "https://api.openai.com/v1"
+client = openai.AsyncClient(api_key=api_key, base_url=endpoint_url)
+
+# https://platform.openai.com/docs/models/gpt-4o
+model_kwargs = {
+    "model": "chatgpt-4o-latest",
+    "temperature": 0.3,
+    "max_tokens": 500
+}
 
 @cl.on_message
 async def on_message(message: cl.Message):
-    # Your custom logic goes here...
+    # Maintain an array of messages in the user session
+    message_history = cl.user_session.get("message_history", [])
+    message_history.append({"role": "user", "content": message.content})
 
-    # Send a response back to the user
-    await cl.Message(
-        content=f"Received: {message.content}",
-    ).send()
+    response_message = cl.Message(content="")
+    await response_message.send()
+
+    # Pass in the full message history for each request
+    stream = await client.chat.completions.create(messages=message_history, 
+                                                  stream=True, **model_kwargs)
+
+    async for part in stream: 
+        if token := part.choices[0].delta.content or "":
+            await response_message.stream_token(token)
+
+    await response_message.update()
